3a46f9894dd5661ed3c7e59363c5589bae6f280e
Pepich <benedikt.abel@yahoo.de>
2016-08-06 18:37:21 +0200

    Totally not a typo fix at all

--- src/de/pepich1851/utils/Main.java
+++ src/de/pepich1851/utils/Main.java
@@ -1,4 +1,5 @@
 package de.pepich1851.utils;
+
 import java.io.File;
 import java.net.URISyntaxException;
 
@@ -7,7 +8,7 @@ import de.pepich1851.plugins.PluginLoader;
 
 public class Main
 {
-	public static File rootDirectorie;
+	public static File rootDirectory;
 	public static final long startup = System.currentTimeMillis();
 	public static final PluginContainer pc = new PluginContainer();
 	
@@ -15,12 +16,14 @@ public class Main
 	{
 		Logger.debugLevel = 1;
 		Logger.info("Starting up Server, locating resources:", "main");
-		rootDirectorie  = new File(Main.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath()).getParentFile();
-		Logger.info("Base server is located at: " + Main.class.getProtectionDomain().getCodeSource().getLocation().toString(), "main");
-		File plugins = new File(rootDirectorie.getPath() + "/plugins");
+		rootDirectory = new File(Main.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath())
+				.getParentFile();
+		Logger.info("Base server is located at: "
+				+ Main.class.getProtectionDomain().getCodeSource().getLocation().toString(), "main");
+		File plugins = new File(rootDirectory.getPath() + "/plugins");
 		Logger.info("Core loaded, loading plugins now", "main");
 		Logger.info("Plugins are located at: " + plugins.getPath(), "main");
 		final PluginLoader pl = new PluginLoader();
-		pl.loadUp(rootDirectorie);
+		pl.loadUp(rootDirectory);
 	}
 }
